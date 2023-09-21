2556cc9405ed8d22ba7ddf23fcb10128ed8da75d
NaN <NaN@DESKTOP-99P4VKM>
2023-09-21 08:42:45 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Logger l1 = Logger.GetInstance();
+        Logger l2 = Logger.GetInstance();
+
+        l1.loggerMessage();
+        l2.loggerMessage();
+
+        if (l1 == l2)
+        {
+            Console.WriteLine("Singleton is working on both instances.");
+        }
+        else
+        {
+            Console.WriteLine("Something went wrong!");
+        }
+
+        Console.ReadLine();
+
+    }
+}
+
+public sealed class Logger
+{
+    private static Logger Instance = new Logger();
+    private Logger() { }
+    public static Logger GetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = new Logger();
+        }
+        return Instance;
+    }
+
+    public void loggerMessage()
+    {
+        Console.WriteLine("Singleton class code has been called!");
+        
+    }
+}
